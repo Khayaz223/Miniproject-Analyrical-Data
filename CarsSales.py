41540d7d2c88985eced6955da2cd1dfd7779edea
Khayaz223 <khairulazhad223@gmail.com>
2025-06-09 16:14:13 +0800

    s

--- CarsSales.py
+++ CarsSales.py
@@ -73,9 +73,7 @@ st.markdown(
     .st-bb, .st-at, .st-ae, .st-af, .st-ag, .st-ah, .st-ai, .st-aj, .st-ak, .st-al, .st-am, .st-an, .st-ao, .st-ap, .st-aq, .st-ar, .st-as {
         background-color: #8B4000;
         color: #ffffff;
-        border: 1px solid #ffffff;
         box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-        padding: 10px;
     }
     .stMetric {
         background-color: #8B4000;
