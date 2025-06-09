b673d935ac2e8a4eb7ae04184356cea77b77ed5d
Khayaz223 <khairulazhad223@gmail.com>
2025-06-09 16:18:39 +0800

    z

--- CarsSales.py
+++ CarsSales.py
@@ -99,6 +99,7 @@ with st.sidebar:
     
     with st.expander("🔧 Select Manufacturer(s)", expanded=False):
         selected_manufacturers = st.multiselect(
+            "Choose from available options",
             options=df['Manufacturer'].unique(),
             default=df['Manufacturer'].unique()
         )
