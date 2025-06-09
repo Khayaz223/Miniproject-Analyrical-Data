9f6a1c11cb0c436b10a983b5c837737ae89b88cb
Khayaz223 <khairulazhad223@gmail.com>
2025-06-09 16:39:46 +0800

    finale

--- CarsSales.py
+++ CarsSales.py
@@ -73,7 +73,7 @@ st.markdown(
         background-color: #00008B;
         color: #ffffff;
         box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-        border-radius: 15px;
+        border-radius: 30px;
     }
     .stMetric {
         background-color: #00008B;
